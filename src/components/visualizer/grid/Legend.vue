44f557840abb027ca2fc64dd74e97069440d009c
Hundway <100788816+Hundway@users.noreply.github.com>
2026-01-03 17:08:27 -0300

    Create a separeate LegendItem component

--- src/components/visualizer/grid/Legend.vue
+++ src/components/visualizer/grid/Legend.vue
@@ -1,17 +1,12 @@
+<script setup lang="ts">
+  import LegendItem from 'LegendItem.vue';
+</script>
+
 <template>
   <ul id="grid-legend">
-    <li>
-      <div class="legend-item start"/>
-      <h3>start</h3>
-    </li>
-    <li>
-      <div class="legend-item end"/>
-      <h3>end</h3>
-    </li>
-    <li>
-      <div class="legend-item wall"/>
-      <h3>wall</h3>
-    </li>
+    <LegendItem title="start" box-class="start"/>
+    <LegendItem title="end"   box-class="end"/>
+    <LegendItem title="wall"  box-class="wall"/>
   </ul>
 </template>
 
@@ -31,21 +26,5 @@
   #grid-legend {
     display: flex;
     gap: 16px;
-  }
-
-  #grid-legend > li {
-    display: flex;
-    align-items: center;
-    gap: 8px;
-  }
-  
-  #grid-legend h3 {
-    color: var(--text);
-  }
-
-  .legend-item {
-    width: 20px;
-    height: 20px;
-    border-radius: 4px;
   }
 </style>
---
+++ src/components/visualizer/grid/LegendItem.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+  const props = defineProps<{title: string, boxClass: string}>()
+</script>
+
+<template>
+  <li>
+    <div :class="props.boxClass" ></div>
+    <h3>{{ title }}</h3>
+  </li>
+</template>
+
+<style scoped>
+  li {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+
+  div {
+    width: 20px;
+    height: 20px;
+    border-radius: 4px;
+  }
+
+  h3 {
+    color: var(--text);
+  }
+</style>

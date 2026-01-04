72718c4fb4a77860bf2eaa5a5d46e88ef456f7dc
Hundway <100788816+Hundway@users.noreply.github.com>
2026-01-03 22:59:31 -0300

    Separate select options in a component

---
+++ src/components/settings/OptionSelectItem.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  const props = defineProps<{'title': string, id: string, 'options': string[]}>()
+</script>
+
+<template>
+  <span>{{ title }}</span>
+  <select :id="props.id">
+    <option v-for="option in options" :value="option" :key="option">{{ option }}</option>
+  </select>
+</template>
+
+<style scoped>
+  span {
+    color: var(--text-muted);
+  }
+
+  select {
+    width: 100%;
+    padding: 6px;
+    border: 1px solid var(--border);
+    border-radius: 8px;
+    background-color: var(--bg-light);
+    color: var(--text);
+  }
+</style>
--- src/components/settings/Options.vue
+++ src/components/settings/Options.vue
@@ -1,53 +1,17 @@
+<script setup lang="ts">
+  import OptionSelectItem from './OptionSelectItem.vue';
+</script>
+
 <template>
   <div>
-    <span>Problem Type</span>
-    <select>
-      <option>8-Puzzle</option>
-      <option>Random Maze</option>
-      <option>Romania Map</option>
-      <option>Simple Grid</option>
-    </select>
-    <span>Search Algorithm </span>
-    <select>
-      <option>Depth-First Search</option>
-      <option>Breadth-First Search</option>
-      <option>Iterative Deepening Search</option>
-      <option>Greedy Best-First Search</option>
-      <option>Uniform Cost Search</option>
-      <option>Dijkstra's</option>
-      <option>A*</option>
-    </select>
-    <span>Heuristic Distance</span>
-    <select>
-      <option>None</option>
-      <option>Manhattan</option>
-      <option>Euclidean</option>
-      <option>Chebyshev</option>
-    </select>
-    <span>Animation Speed </span>
-    <select>
-      <option>Slow</option>
-      <option>Normal</option>
-      <option>Fast</option>
-      <option>Instant</option>
-    </select>
+    <OptionSelectItem title="Problem Type" id="problem-type" :options="['8-Puzzle', 'Random Maze', 'Romania Map', 'Simple Grid']"/>
+    <OptionSelectItem title="Search Algorithm" id="search-algorithm" :options="['Depth-First Search', 'Breadth-First Search', 'Iterative Deepening Search', 'Greedy Best-First Search', 'Uniform Cost Search', 'A*']"/>
+    <OptionSelectItem title="Heuristic Distance" id="heristic" :options="['None', 'Manhattan', 'Euclidean', 'Chebyshev']"/>
+    <OptionSelectItem title="Animation Speed" id="animation-speed" :options="['Slow', 'Normal', 'Fast', 'Instant']"/>
   </div>
 </template>
 
 <style scoped>
-  span {
-    color: var(--text-muted);
-  }
-
-  select {
-    width: 100%;
-    padding: 6px;
-    border: 1px solid var(--border);
-    border-radius: 8px;
-    background-color: var(--bg-light);
-    color: var(--text);
-  }
-
   div {
     display: flex;
     flex-direction: column;

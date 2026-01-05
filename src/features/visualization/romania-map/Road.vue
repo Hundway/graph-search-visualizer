d529cfb577587a2436e16d7e729e12ea568cdb0f
Hundway <100788816+Hundway@users.noreply.github.com>
2026-01-04 22:07:12 -0300

    Remove unused code

--- src/features/visualization/romania-map/Road.vue
+++ src/features/visualization/romania-map/Road.vue
@@ -1,37 +1,18 @@
 <script setup lang="ts">
-  const props = defineProps<{
-    x1: number,
-    y1: number,
-    x2: number,
-    y2: number,
-  }>()
-  const midX = (props.x1 + props.x2) / 2;
-  const midY = (props.y1 + props.y2) / 2;
+  const props = defineProps<{x1: number, y1: number, x2: number, y2: number,}>()
 </script>
 
 <template>
-  <g class="road-group">
-    <line 
-      :x1="x1" :y1="y1" 
-      :x2="x2" :y2="y2" 
-      class="road-line" 
-    />
-  </g>
+  <line 
+    :x1="x1" :y1="y1" 
+    :x2="x2" :y2="y2" 
+    class="road-line" 
+  />
 </template>
 
 <style scoped>
   .road-line {
     stroke: var(--border);
     stroke-width: 2px;
-  }
-
-  .label-bg {
-    fill: var(--bg-dark);
-    stroke-width: 1px;
-  }
-
-  .road-label {
-    font-size: 24px;
-    fill: var(--text);
   }
 </style>

bc012110e33c3891cb9bceaa168506108fc5f736
Hundway <100788816+Hundway@users.noreply.github.com>
2026-01-05 00:00:42 -0300

    Create a header component

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  const props = defineProps<{title: string}>()
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+  h1 {
+    font-size: 42px;
+    color: var(--text);
+    text-shadow: 2px 2px 2px var(--text-muted);
+  }
+</style>

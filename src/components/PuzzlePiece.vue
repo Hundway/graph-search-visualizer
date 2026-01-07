ab0705aa4d30290f744c54272b33661e4c7d8a2d
Hundway <100788816+Hundway@users.noreply.github.com>
2026-01-06 23:12:50 -0300

    Create initial puzzle view

---
+++ src/components/PuzzlePiece.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+  const props = defineProps<{title: number | string}>()
+</script>
+
+<template>
+  <div class="puzzle-piece">
+    <span>{{ title }}</span>
+  </div>
+</template>
+
+<style scoped>
+  .puzzle-piece {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 150px;
+    height: 150px;
+    border: 2px solid var(--border);
+    border-radius: 4px;
+    background-color: var(--bg);
+  }
+
+  .puzzle-piece span{
+    font-size: 48px;
+    color: var(--text);
+    text-shadow: 2px 2px 2px var(--text-muted);
+  }
+</style>
--- src/features/visualizer/eight-puzzle/PuzzleView.vue
+++ src/features/visualizer/eight-puzzle/PuzzleView.vue
@@ -1,5 +1,32 @@
 <script setup lang="ts">
+  import Header from '@/components/Header.vue';
+  import PuzzlePiece from '@/components/PuzzlePiece.vue';
 </script>
 
 <template>
+  <main id="puzzle-view">
+    <Header title="8-Puzzle" />
+    <div class="puzzle">
+      <PuzzlePiece title=""/>
+      <PuzzlePiece v-for="value in 8" :title="value"/>
+    </div>
+  </main>
 </template>
+
+<style scoped>
+  #puzzle-view {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+
+  .puzzle {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    gap: 8px;
+    padding: 10px;
+    border: 2px solid var(--border);
+    border-radius: 4px;
+  }
+</style>
